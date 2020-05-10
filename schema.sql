b530cec23399282e7829c6c18569d26e92bea31b
William Rave <wrave@gwu.edu>
2020-05-10 16:03:11 -0400

    Employee table written

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS employes_db;
-CREATE DATABASE employes_db;
-USE employes_db;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+USE employees_db;
 
 CREATE TABLE departments(
     id INTEGER NO NULL AUTO_INCREMENT,
@@ -11,7 +11,16 @@ CREATE TABLE departments(
 CREATE TABLE roles(
     id INTEGER NO NULL AUTO_INCREMENT,
     roleName VARCHAR NO NULL,
-    salary DECIMAL,
-    department_id INTEGER,
+    salary DECIMAL NO NULL,
+    department_id INTEGER NO NULL,
     PRIMARY KEY(id)
-)
+);
+
+CREATE TABLE employees(
+    id INTEGER NO NULL AUTO_INCREMENT,
+    first_name VARCHAR NO NULL,
+    last_name VARCHAR NO NULL,
+    role_id INTEGER NO NULL,
+    manager_id INTEGER,
+    PRIMARY KEY(id)
+);
