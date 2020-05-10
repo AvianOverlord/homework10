3ad8a28a79c26f6c11fc9eca1ce6838af07a3b60
William Rave <wrave@gwu.edu>
2020-05-10 15:57:22 -0400

    Database creation and department table

---
+++ schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS employes_db;
+CREATE DATABASE employes_db;
+USE employes_db;
+
+CREATE TABLE departments(
+    id INTEGER NO NULL AUTO_INCREMENT,
+    departmentName VARCHAR NO NULL
+);
+
