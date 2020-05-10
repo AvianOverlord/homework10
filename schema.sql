627a9bab04bf47ba8c2b852d2285ea27c0113baa
William Rave <wrave@gwu.edu>
2020-05-10 16:27:09 -0400

    Fixed SQL syntax errors

--- schema.sql
+++ schema.sql
@@ -3,24 +3,24 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE departments(
-    id INTEGER NO NULL AUTO_INCREMENT,
-    departmentName VARCHAR NO NULL,
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    departmentName VARCHAR(30),
     PRIMARY KEY(id)
 );
 
 CREATE TABLE roles(
-    id INTEGER NO NULL AUTO_INCREMENT,
-    roleName VARCHAR NO NULL,
-    salary DECIMAL NO NULL,
-    department_id INTEGER NO NULL,
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    roleName VARCHAR(30),
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE employees(
-    id INTEGER NO NULL AUTO_INCREMENT,
-    first_name VARCHAR NO NULL,
-    last_name VARCHAR NO NULL,
-    role_id INTEGER NO NULL,
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    role_id INTEGER NOT NULL,
     manager_id INTEGER,
     PRIMARY KEY(id)
 );
