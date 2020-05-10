e3e5447f810a46745a2ba3f1372a5a7413d93ecc
William Rave <wrave@gwu.edu>
2020-05-10 16:00:12 -0400

    role table written

--- schema.sql
+++ schema.sql
@@ -4,6 +4,14 @@ USE employes_db;
 
 CREATE TABLE departments(
     id INTEGER NO NULL AUTO_INCREMENT,
-    departmentName VARCHAR NO NULL
+    departmentName VARCHAR NO NULL,
+    PRIMARY KEY(id)
 );
 
+CREATE TABLE roles(
+    id INTEGER NO NULL AUTO_INCREMENT,
+    roleName VARCHAR NO NULL,
+    salary DECIMAL,
+    department_id INTEGER,
+    PRIMARY KEY(id)
+)
