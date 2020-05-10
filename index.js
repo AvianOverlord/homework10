84ff326f4002a8b513f7a94625c4faef87883fc3
William Rave <wrave@gwu.edu>
2020-05-10 16:29:33 -0400

    Main choice function

--- index.js
+++ index.js
@@ -9,6 +9,35 @@ let connection = mysql.createConnection({
     database: "employees_db"
 })
 
-function Init(){
-
+function MainChoice(){
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "mainChoice",
+            message: "What would you like to do?",
+            choices: ["Add a new department","View your departments","Add a new employee role","View employee roles","Add an employee","View current employees","Update an employee's role","Quit"]
+        }
+    ]).then(data =>{
+        switch(data.mainChoice)
+        {
+            case "Add a new department":
+                break;
+            case "View your departments":
+                break;
+            case "Add a new employee role":
+                break;
+            case "View employee roles":
+                break;
+            case "Add an employee":
+                break;
+            case "View your employees":
+                break;
+            case "Update an employee's role":
+                break;
+            case "Quit":
+                connection.end();
+                break;
+        }   
+    })
 }
+MainChoice();
